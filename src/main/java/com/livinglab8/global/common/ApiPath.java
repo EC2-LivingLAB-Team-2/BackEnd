c4838a7fd71f0341527b6ce85846a25e493ca0fc
HP657 <junlee5070@gmail.com>
2025-05-09 22:23:28 +0900

    ApiPath 설정

---
+++ src/main/java/com/livinglab8/global/common/ApiPath.java
@@ -0,0 +1,8 @@
+package com.livinglab8.global.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiPath {
+    public static final String API_ROOT = "/api/v1";
+}
